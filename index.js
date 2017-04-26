1236f359f35246f76cb5caa4e786389bef13ae73
Tim Marinin <mt@marinin.xyz>
2017-04-27 00:14:45 +0300

    Refactor index.js

--- index.js
+++ index.js
@@ -2,20 +2,40 @@
 const http = require('http')
 const https = require('https')
 
-const api_key = process.env.DARKSKY_API || process.argv[2]
+let api_key = process.env.DARKSKY_API || process.argv[2]
 
-if (!api_key) {
-  console.error('Please, enter your api_key either as DARKSKY_API env variable or CLI argument')
-  process.exit(127)
+const addApiKey = (url, apiKey) => {
+  url = url.slice(1).split('/')
+  url = ['https://api.darksky.net', url[0], apiKey].concat(url.slice(1)).join('/')
+  return url
 }
 
-const server = http.createServer((req, res) => {
-  let url = req.url.slice(1).split('/')
-  url = ['https://api.darksky.net', url[0], api_key].concat(url.slice(1)).join('/')
-  https.get(url, (dsRes) => dsRes.pipe(res)).pipe(req)
-})
+const proxy = (req, res) => {
+  let url = addApiKey(req.url)
+  return https.get(url, (dsRes) => dsRes.pipe(res)).pipe(req)
+}
 
-const port = process.env.PORT || 8050
-server.listen(port, () => {
-  console.log('Powered by DarkSky. Go ahead, try it! Proxy is available at http://localhost:' + port)
-})
+const handleRequest = (req, res) => {
+  if (req.method === 'GET') return proxy(req, res)
+  else return res.end(`Unsupported method ${req.method}`)
+}
+
+if (!module.parent) {
+  if (!api_key) {
+    console.error('Please, enter your api_key either as DARKSKY_API env variable or CLI argument')
+    process.exit(127)
+  }
+
+  const server = http.createServer(handleRequest)
+
+  const port = process.env.PORT || 8050
+  server.listen(port, () => {
+    console.log('Powered by DarkSky. Go ahead, try it! Proxy is available at http://localhost:' + port)
+  })
+}
+
+module.exports = {
+  proxy,
+  handleRequest,
+  addApiKey
+}
